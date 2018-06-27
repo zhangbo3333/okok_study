bcf435d74f735511b8990619ac8a2a2f17a79cf6
zhangbo3333 <funy_suny@163.com>
2018-06-27 14:56:41 +0800

    x

--- easyswoole/Application/HttpController/Lib/Sms/Support/Sms.php
+++ easyswoole/Application/HttpController/Lib/Sms/Support/Sms.php
@@ -3,7 +3,7 @@ namespace App\HttpController\Lib\Sms\Support;
 use Aws\Ses\SesClient;
 use Aws\Sns\SnsClient;
 use Aws\Ses\Exception\SesException;
-
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Sms extends BaseSms {
     public function send($phone,$code)
@@ -111,45 +111,112 @@ class Sms extends BaseSms {
 //
 //        ));
 
-        try {
-            $result = $snsClient->sendEmail([
-                'Destination' => [
-                    'ToAddresses' => [
-                        'noreply@service.bitboole.com',
-                    ],
-                ],
-                'Message' => [
-                    'Body' => [
-                        'Html' => [
-                            'Charset' => 'UTF-8',
-                            'Data' => '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
-                                '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
-                                'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
-                                'AWS SDK for PHP</a>.</p>',
-                        ],
-                        'Text' => [
-                            'Charset' => 'UTF-8',
-                            'Data' => '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
-                                '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
-                                'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
-                                'AWS SDK for PHP</a>.</p>',
-                        ],
-                    ],
-                    'Subject' => [
-                        'Charset' => 'UTF-8',
-                        'Data' => 'Amazon SES test (AWS SDK for PHP)',
-                    ],
-                ],
-                'Source' => 'noreply@service.bitboole.com',
-                // If you are not using a configuration set, comment or delete the
-                // following line
-//                'ConfigurationSetName' => 'ConfigSet',
-            ]);
-            $messageId = $result->get('MessageId');
-            echo("Email sent! Message ID: $messageId"."\n");
+//        try {
+//            $result = $snsClient->sendEmail([
+//                'Destination' => [
+//                    'ToAddresses' => [
+//                        'noreply@service.bitboole.com',
+//                    ],
+//                ],
+//                'Message' => [
+//                    'Body' => [
+//                        'Html' => [
+//                            'Charset' => 'UTF-8',
+//                            'Data' => '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+//                                '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+//                                'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+//                                'AWS SDK for PHP</a>.</p>',
+//                        ],
+//                        'Text' => [
+//                            'Charset' => 'UTF-8',
+//                            'Data' => '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+//                                '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+//                                'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+//                                'AWS SDK for PHP</a>.</p>',
+//                        ],
+//                    ],
+//                    'Subject' => [
+//                        'Charset' => 'UTF-8',
+//                        'Data' => 'Amazon SES test (AWS SDK for PHP)',
+//                    ],
+//                ],
+//                'Source' => 'noreply@service.bitboole.com',
+//                // If you are not using a configuration set, comment or delete the
+//                // following line
+////                'ConfigurationSetName' => 'ConfigSet',
+//            ]);
+//            $messageId = $result->get('MessageId');
+//            echo("Email sent! Message ID: $messageId"."\n");
+//
+//        } catch (SesException $error) {
+//            echo("The email was not sent. Error message: ".$error->getAwsErrorMessage()."\n");
+//        }
 
-        } catch (SesException $error) {
-            echo("The email was not sent. Error message: ".$error->getAwsErrorMessage()."\n");
+        // Modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+//        require 'path_to_sdk_inclusion';
+
+// Instantiate a new PHPMailer
+        $mail = new PHPMailer;
+
+// Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+        $mail->setFrom('noreply@service.bitboole.com', 'Sender Name');
+
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+// Also note that you can include several addAddress() lines to send
+// email to multiple recipients.
+        $mail->addAddress('funy_suny@163.com', 'Recipient Name');
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+        $mail->Username = 'AKIAJE353SOUXYK5P4UA';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+        $mail->Password = 'AhTf8eokqpm+buQszf7Fa80/33bJbqLMUWm2NRwKzM3f';
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+        $mail->addCustomHeader('X-SES-CONFIGURATION-SET', 'ConfigSet');
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+        $mail->Host = 'email-smtp.us-west-2.amazonaws.com';
+
+// The subject line of the email
+        $mail->Subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The HTML-formatted body of the email
+        $mail->Body = '<h1>Email Test</h1>
+    <p>This email was sent through the 
+    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+// Tells PHPMailer to use SMTP authentication
+        $mail->SMTPAuth = true;
+
+// Enable TLS encryption over port 587
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+// Tells PHPMailer to send HTML-formatted email
+        $mail->isHTML(true);
+
+// The alternative email body; this is only displayed when a recipient
+// opens the email in a non-HTML email client. The \r\n represents a
+// line break.
+        $mail->AltBody = "Email Test\r\nThis email was sent through the 
+    Amazon SES SMTP interface using the PHPMailer class.";
+
+        if(!$mail->send()) {
+            echo "Email not sent. " , $mail->ErrorInfo , PHP_EOL;
+        } else {
+            echo "Email sent!" , PHP_EOL;
         }
         return ['phone'=>$phone,'code'=>$code,'provider'=>'aws','result'=>'ok'];
     }
