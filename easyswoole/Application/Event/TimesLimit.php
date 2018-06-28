f260f595d2d10ace856941a83bdfa40f2d45a843
zhangbo3333 <funy_suny@163.com>
2018-06-28 10:39:01 +0800

    x

--- easyswoole/Application/Event/TimesLimit.php
+++ easyswoole/Application/Event/TimesLimit.php
@@ -29,10 +29,10 @@ class TimesLimit {
     {
         $redis = new \Redis();
         $redis->connect('127.0.0.1',6379);
+        var_dump($this->getPrefix().$this->address);
+        var_dump($redis->exists($this->getPrefix().$this->address));
         if($redis->exists($this->getPrefix().$this->address)){
-            var_dump('存在键');
             $num = $redis->get($this->getPrefix().$this->address);
-            var_dump($num);
             $redis->set($this->getPrefix().$this->address,$num+1);
         }else{
             $redis->setex($this->getPrefix().$this->address,$this->cycle,1);
