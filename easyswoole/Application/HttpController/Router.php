5f3874459f19c6d80074929b467f3e2ff659abf5
zhangbo3333 <funy_suny@163.com>
2018-06-29 11:30:25 +0800

    x

--- easyswoole/Application/HttpController/Router.php
+++ easyswoole/Application/HttpController/Router.php
@@ -17,29 +17,7 @@ class Router extends \EasySwoole\Core\Http\AbstractInterface\Router
 {
     function register(RouteCollector $routeCollector)
     {
-        // TODO: Implement register() method.
-        //路由拦截若未执行end，请求还会继续进入系统自带的控制器路由匹配
 
-
-        //测试URL  /a
-        $routeCollector->get('/a',function (Request $request,Response $response){
-            $response->write('this is write by router with not end ');
-        });
-
-        //测试URL  /a2
-        $routeCollector->get('/a2',function (Request $request,Response $response){
-            $response->write('this is write by router2 with end ');
-            $response->end();
-        });
-
-        // /user/1/index.html
-        $routeCollector->get( '/user/{id:\d+}',function (Request $request ,Response $response,$id){
-            $response->write("this is router user ,your id is {$id}");
-            $response->end();
-        });
-
-        //传递给 /index控制器 test2方法
-//        $routeCollector->get( '/user2/{id:\w+}','/test2');
     }
 
 }
