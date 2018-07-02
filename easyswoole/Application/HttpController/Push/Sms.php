e233a56269979414a190b143cee841fc0de7629a
zhangbo3333 <funy_suny@163.com>
2018-07-02 11:58:33 +0800

    x

--- easyswoole/Application/HttpController/Push/Sms.php
+++ easyswoole/Application/HttpController/Push/Sms.php
@@ -14,7 +14,7 @@ class Sms extends Controller {
 
 
     public function index(){
-        $this->response()->write('123');
+        $this->response()->write('123123456');
         var_dump('123');
         var_dump(Capsule::table('test')->insertGetId(['name'=>'张三','age'=>12]));
         var_dump('456');
